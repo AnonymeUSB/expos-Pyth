d4e130944fb99e577cfb79df7046503006052d5a
AnonymeUSB <Allyssisation@gmail.com>
2022-09-29 17:25:25 +0000

    musique

---
+++ script.js
@@ -0,0 +1,19 @@
+function Musique1() {
+    var musique1 = new Audio('Cloud-Sky.mp3');
+    musique1.play();
+}
+
+function Musique2() {
+    var musique2 = new Audio('feel-good.mp3');
+    musique2.play();
+}
+
+function Musique3() {
+    var musique3 = new Audio('Epoch.mp3');
+    musique3.play();
+}
+
+function Musique4() {
+    var musique4 = new Audio('Summertime.mp3');
+    musique4.play();
+}
